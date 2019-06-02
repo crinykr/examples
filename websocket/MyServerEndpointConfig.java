8455a329793701f8e22200b2c77470c91548b771
crinykr <crinykr@gmail.com>
2019-06-02 20:36:46 +0900

    Create MyServerEndpointConfig.java

---
+++ websocket/MyServerEndpointConfig.java
@@ -0,0 +1,12 @@
+package web_test;
+
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+
+public class MyServerEndpointConfig extends ServerEndpointConfig.Configurator {
+	@Override
+	public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
+		config.getUserProperties().put("MyHttpHeaders", request.getHeaders());
+	}
+}
